505f8354292eda9f9c5aa7d9c8498c7ce889bb31
codetony25 <me@codetony.com>
2018-08-12 03:25:52 -0700

    updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const port = process.env.PORT || 5000;
-export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
+export const port = process.env.PORT || 8000;
+export const host = process.env.WEBSITE_HOSTNAME || `codeforappscom.herokuapp.com:${port}`;
 
 export const analytics = {
 
